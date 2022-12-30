65351292bf493442d7372cc8c6369b0b6836b64a
Blackmayweather <68154160+Blackmayweather@users.noreply.github.com>
2022-12-30 19:02:05 +0100

    fixed ts files

--- test/lex.h
+++ test/lex.h
@@ -1,5 +1,5 @@
 #ifndef LEX_H
-	#define LEX_H
+#define LEX_H
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -7,14 +7,8 @@
 int yylineo=1; //compter le nombre de ligne
 int Col=1; //nombre de colonne
 
-
 void pos(){
 	printf("%d,%d  ",yylineo,Col);
 }
-void Fpos(FILE *fp){
-	fprintf(fp,"%d,%d  ",yylineo,Col);
-}
 
-
-    
 #endif
